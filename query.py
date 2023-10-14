622ea034a69ac8998e62a15db17c9c2f3a3a15f4
hannahdoret <22846377@student.uwa.edu.au>
2023-10-15 02:24:57 +0800

    add another query

--- query.py
+++ query.py
@@ -7,26 +7,61 @@ g.parse("handbook.ttl", format="turtle")
 
 arguments = sys.argv
 
-#if arguments[1] == "getAllMajors":
-#    selection = "?major"
-#    query = "?major a handbook:major ."
+if arguments[1] == "getAllMajors":
+    selection = "?major"
+    query = "?major a handbook:major ."
 
-#results = default_world.sparql(
-#    f"""
-#        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-#        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-#        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-#        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-#        PREFIX pokemon: <http://test.org/pokemon#>
+results = default_world.sparql(
+    f"""
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX pokemon: <http://test.org/pokemon#>
 
-#        SELECT {selection}
-#        WHERE{{
-#        {query}
-#        }}
-#        """)
+        SELECT {selection}
+        WHERE{{
+        {query}
+        }}
+        """)
 
-#for result in results:
-#    print(result)
+for result in results:
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -42,7 +77,8 @@ arguments = sys.argv
 #############################           hannah         #########################
 # find all level 3 units that do not have an exam, and none of their prerequisites have an exam either
 han_query = """
-    SELECT ?unit WHERE {
+    SELECT ?unit WHERE 
+    {
         ?unit handbook:has_level handbook:3 .
         FILTER NOT EXISTS {?unit handbook:has_assessment handbook:exam} .
         OPTIONAL {?unit handbook:has_prerequisite ?prereq} .
@@ -51,7 +87,28 @@ han_query = """
     GROUP BY ?unit
 """
 
-han_result = g.query(han_query)
-for result in han_result:
-    print(result)
+# find units that contain <search_string> in the descriptions or outcomes
+# search string should probably come from user input
+search_string = "amazing"
+han_query2 = f"""
+    SELECT ?unit WHERE
+    {{
+        {{
+            ?unit a handbook:unit .
+            ?unit handbook:has_description ?description .
+            FILTER CONTAINS(?description, "{search_string}") .
+        }}
+        UNION
+        {{
+            ?unit a handbook:unit .
+            ?unit handbook:has_outcome ?outcome .
+            FILTER CONTAINS(?outcome, "{search_string}") .
+        }}
+    }}
+    GROUP BY ?unit
+"""
+
+han_result = g.query(han_query2)
+#for result in han_result:
+#    print(result)
     
