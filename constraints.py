2d4f1dd65a69694c55e4df08f5bbb95d8d706a47
hannahdoret <22846377@student.uwa.edu.au>
2023-10-17 15:33:15 +0800

    working constraint

--- constraints.py
+++ constraints.py
@@ -3,67 +3,30 @@ from pyshacl import validate
 
 data_graph = Graph()
 data_graph.parse("handbook.ttl", format="ttl")
-# sha_str = """
-#             @prefix sha: <http://www.w3.org/ns/shacl#> .
-#             @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-#             @prefix h: <http://university.org/> .
-
-#             h:unit_shape a sha:NodeShape ;
-#                 sha:targetClass h:unit ;
-
-#             sha:property [
-#                 sha:path h:has_level ;
-#                 sha:property [
-#                     sha:path h:has_number ;
-#                     sha:datatype xsd:integer ;
-#                 ] ;
-#             ] ;
-#             sha:property [
-#                 sha:path h:has_prerequisite ;
-#                 sha:qualifiedValueShape [
-#                     sha:nodeKind sha:IRI ;
-#                     sha:property [
-#                         sha:path h:has_level ;
-#                         sha:property [
-#                             sha:path h:has_number ;
-#                             sha:maxExclusive 9 ;
-#                         ] ;
-#                     ] ;
-#                 ] ;
-#                 sha:qualifiedMinCount 1 ;
-#             ] .
-
-# """
-
 
 sha_str = """
             @prefix sha: <http://www.w3.org/ns/shacl#> .
             @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
             @prefix h: <http://university.org/> .
 
-            h:prefixDeclarations
-	            sha:declare [
-		        sha:prefix "h" ;
-		        sha:namespace "http://university.org/" ;
-	        ] .
+
 
             h:unit_shape a sha:NodeShape ;
                 sha:targetClass h:unit ;
-            sha:sparql [
-                sha: prefixes h:prefixDeclarations ;
-		        sha:select '''
-                    SELECT $this ?preUnit 
-                    WHERE { 
-                        $this h:has_prerequisite ?preUnit . 
-                        $this h:has_level ?thisLevel . 
-                        ?thisLevel h:has_number ?thisNumber . 
-                        ?preUnit h:has_level ?preLevel . 
-                        ?preLevel h:has_number ?preNumber . 
-                        FILTER (?thisNumber <= ?preNumber) .
-                    } 
-                ''' ;
-                sha:message "The level of a unit must be higher than its prerequisite." ;
-	        ] ; .
+                sha:sparql [
+		            sha:select '''
+                    PREFIX h: <http://university.org/>
+                        SELECT $this ?preUnit 
+                        WHERE { 
+                            $this h:has_prerequisite ?preUnit . 
+                            $this h:has_level ?thisLevel . 
+                            ?thisLevel h:has_number ?thisNumber . 
+                            ?preUnit h:has_level ?preLevel . 
+                            ?preLevel h:has_number ?preNumber . 
+                            FILTER (?thisNumber <= ?preNumber) .
+                        } 
+                    ''' ;
+	        ] . 
 """
 
 
