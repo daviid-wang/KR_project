3ee805df10951b4c3de52c6d90c42e34ba7f8f7b
David <david.holt2002@gmail.com>
2023-10-22 11:40:16 +0800

    works with 5

--- ontology.py
+++ ontology.py
@@ -18,7 +18,6 @@ with onto:
     class School(Thing): pass
     class BoardofExaminers(Thing): pass
     class DeliveryMode(Thing): pass
-    class Level(Thing): pass
     class Assessment(Thing): pass
     class Major(Thing): pass
     class Course(Thing): pass
@@ -33,7 +32,7 @@ with onto:
     class has_school(Unit >> School): pass
     class has_board_of_examiners(Unit >> BoardofExaminers): pass
     class has_unit_delivery_mode(Unit >> DeliveryMode): pass
-    class has_level(Unit >> Level): pass
+    class has_level(Unit >> int, DataProperty): pass
     class has_description(Unit >> str, DataProperty): pass
     class has_outcome(Unit >> str, DataProperty): pass
     class has_assessment(Unit >> Assessment): pass
@@ -55,25 +54,23 @@ with onto:
     #has_number = h.has_number # used to give level objects an integer literal, bc integers are easier to use in SHACL
 
     # CITS3200 = Unit("CITS3200", has_title=[units_data["CITS3200"]['title']])
-    delivery_m = []
+    data_stuff = []
     
-    
-    #Add unit title for every unit
+    #Add unit information for every unit
     for unit_name in units_data:
-        #data_stuff = []
-        # if units_data[unit_name]["delivery_mode"] not in data_stuff:
-        #     data_stuff.append(units_data[unit_name]["delivery_mode"])
+        # if units_data[unit_name]["level"] not in data_stuff:
+        #     data_stuff.append(int(units_data[unit_name]["level"]))
         #Add unit delivery mode if the unit has one
         delivery_m = ""
         if units_data[unit_name]["delivery_mode"] != "":
-            delivery_m = f'has_unit_delivery_mode=[DeliveryMode("{units_data[unit_name]["delivery_mode"]}")]'
+            delivery_m = f'has_unit_delivery_mode=[DeliveryMode("{units_data[unit_name]["delivery_mode"]}")],'
         unit_add = f'''{unit_name} = \
             Unit("{unit_name}", 
                 has_title=["{units_data[unit_name]["title"]}"], 
                 has_school=[School("{units_data[unit_name]["school"]}")],
                 has_board_of_examiners=[BoardofExaminers("{units_data[unit_name]["board_of_examiners"]}")],
                 {delivery_m}
-                
+                has_level=[{int(units_data[unit_name]["level"])}]
             )
         '''
         # print(unit_add)
